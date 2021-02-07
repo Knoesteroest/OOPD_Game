ca1b3a635f3ba73febc2aaadc928c0b32a977412
Knoesteroest <72971554+Knoesteroest@users.noreply.github.com>
2021-02-07 15:25:21 +0100

    Verandert van richting als je op spatie drukt

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,13 @@
 import nl.han.ica.oopg.objects.GameObject;
 import processing.core.PGraphics;
 
-public class Player extends GameObject{
+public class Player extends GameObject {
 
     final int kleur = 0xFF0000FF;
-    public Player(float x, float y, float width, float height){
-        super( x,  y,  width,  height);
-        setDirectionSpeed(0,2);
+
+    public Player(float x, float y, float width, float height) {
+        super(x, y, width, height);
+        setDirectionSpeed(0, 2);
     }
 
     @Override
@@ -18,6 +19,23 @@ public class Player extends GameObject{
     public void draw(PGraphics pGraphics) {
         pGraphics.fill(kleur);
         pGraphics.rectMode(CENTER);
-        pGraphics.rect(x,y,width,height);
+        pGraphics.rect(x, y, width, height);
+    }
+
+    @Override
+    public void keyPressed(int keyCode, char key) {
+        if (keyCode==32){
+            setDirection((float) (Math.random()*360));
+        }
+
+        /*
+        {UP, DOWN, LEFT, RIGHT}
+        {87, 83, 65, 68}
+
+         */
+    }
+
+    @Override
+    public void keyReleased(int keyCode, char key) {
     }
 }
