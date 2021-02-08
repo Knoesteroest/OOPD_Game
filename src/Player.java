35b5ebb3fe9064f8094f6401364d604e0d806a15
Knoesteroest <72971554+Knoesteroest@users.noreply.github.com>
2021-02-08 12:14:59 +0100

    Beweegt met de pijltjestoetsen.

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,41 @@
 import nl.han.ica.oopg.objects.GameObject;
 import processing.core.PGraphics;
+import processing.data.IntList;
+import processing.core.PConstants;
+
+import java.lang.reflect.Array;
 
 public class Player extends GameObject {
 
     final int kleur = 0xFF0000FF;
+    IntList pressedKeys = new IntList();
 
     public Player(float x, float y, float width, float height) {
         super(x, y, width, height);
-        setDirectionSpeed(0, 2);
     }
 
     @Override
     public void update() {
-
+        if (pressedKeys.size()>0) {
+            System.out.println(pressedKeys);
+            int lastKey = pressedKeys.get(pressedKeys.maxIndex());
+            switch (lastKey) {
+                case UP:
+                    setDirectionSpeed(0, 2);
+                    break;
+                case DOWN:
+                    setDirectionSpeed(180, 2);
+                    break;
+                case LEFT:
+                    setDirectionSpeed(270, 2);
+                    break;
+                case RIGHT:
+                    setDirectionSpeed(90, 2);
+                    break;
+            }
+        }else{
+            setSpeed(0);
+        }
     }
 
     @Override
@@ -24,18 +47,35 @@ public class Player extends GameObject {
 
     @Override
     public void keyPressed(int keyCode, char key) {
-        if (keyCode==32){
+        /*if (keyCode==32){
             setDirection((float) (Math.random()*360));
+        }
+*/
+        switch(keyCode){
+            case UP:
+            case DOWN:
+            case LEFT:
+            case RIGHT:
+
+                pressedKeys.append(keyCode); //appendUnique bestaat niet?
         }
 
         /*
         {UP, DOWN, LEFT, RIGHT}
-        {87, 83, 65, 68}
+        {87, 83, 65, 68} WSAD
 
          */
     }
 
     @Override
     public void keyReleased(int keyCode, char key) {
+        switch(keyCode){
+            case UP:
+            case DOWN:
+            case LEFT:
+            case RIGHT:
+                pressedKeys.removeValues(keyCode);
+        }
     }
+
 }
