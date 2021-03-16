bb171a4690bfef9d815be5c76bbdce4db242e7f0
Martijnme <47241809+Martijnme@users.noreply.github.com>
2021-03-16 11:54:45 +0100

    Add files via upload

---
+++ src/tiles/WallTile.java
@@ -0,0 +1,14 @@
+package tiles;
+
+import me.main.Game;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+import nl.han.ica.oopg.tile.TileMap;
+import nl.han.ica.oopg.tile.TileType;
+
+public class WallTile extends Tile {
+    public WallTile(Sprite sprite) {
+        super(sprite);
+
+    }
+}
